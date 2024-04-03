a7c775d53422a9010f8b22ac6aba26e4913ae2f5
Killian Lecrut <killianlecrut@MacBook-Air-de-Killian.local>
2024-04-03 21:34:11 +0200

    debut parallax

--- logo.js
+++ logo.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         document.querySelector('.logo').style.display = 'none';
         document.querySelector('.content').classList.remove('blurred');
-    }, 3000); // Après 3 secondes, le logo disparaît et le flou est retiré.
+    }, 30); // Après 3 secondes, le logo disparaît et le flou est retiré.
 });
