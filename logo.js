c8081420835d3c05f8a6baf9808e93c539b62f8b
Killian Lecrut <killianlecrut@MacBook-Air-de-Killian.local>
2024-04-13 16:58:26 +0200

    Premier design

--- logo.js
+++ logo.js
@@ -4,3 +4,26 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector('.content').classList.remove('blurred');
     }, 30); // Après 3 secondes, le logo disparaît et le flou est retiré.
 });
+
+
+
+
+$(document).ready(function() {
+  
+    $('button').on('click', function() {
+      if($(this).hasClass('nav-button')) {
+        $('nav div').addClass('show');
+      } else if($(this).hasClass('exit-menu')) {
+        $('nav div').removeClass('show');
+      } 
+      else if($(this).hasClass('to-top')) {
+        $('html,body').animate({scrollTop:0}, 'slow');
+      }
+    });
+  
+    AOS.init({      
+          duration: 1800,
+      easing: 'ease'
+    });
+     
+  })
